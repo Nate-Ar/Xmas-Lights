be105bbcfe0a53d10fe4c24aa304e076656f7def
Nate-Ar <45805730+Nate-Ar@users.noreply.github.com>
2020-11-18 10:35:54 -0600

    Commit 1

--- Raspberry-Pi/moter.py
+++ Raspberry-Pi/moter.py
@@ -1,14 +1,17 @@
 from gpiozero import Servo
 from time import sleep
 
+servo = Servo(4)
+
 
 def arm():
-    servo = Servo(4)
     servo.min()
     sleep(2)
     servo.max()
     sleep(2)
     servo.mid()
     sleep(1)
-
+def spin():
+    servo.max()
 arm()
+
