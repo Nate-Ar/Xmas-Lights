276bfda0e15e94ff482a82375498b7feae66e470
Nate-Ar <45805730+Nate-Ar@users.noreply.github.com>
2020-11-22 14:41:32 -0600

    Commit 1

--- Raspberry-Pi/PiLIghts.py
+++ Raspberry-Pi/PiLIghts.py
@@ -6,9 +6,23 @@
 from gpiozero import LED
 import os
 from time import sleep
+import librosa, librosa.display
+from random import randint
+import matplotlib.pyplot as plt
+import time
+
+
+def analze(file):
+    file = file
+    signal, sr = librosa.load(file, sr=22050)
+    librosa.display.waveplot(signal, sr=sr)
+
+    y, sr = librosa.load(file)
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+    return round(tempo/60, 1)
 
 # on and off
-
 def slot1(x):
     slot1 = LED(4)
     slot1.on()
@@ -50,17 +64,32 @@ def slot8(x):
     sleep(x)
     slot8.off()
 
-# songs
-def song1():
-    while True:
-        slot8(.5)
-        sleep(.5)
-        slot7(.5)
-        sleep(.5)
-        slot6(.5)
-        sleep(.5)
-        slot5(.5)
-        sleep(.5)
 
-# run
-song1()
+def rooftop():
+    tempo = analze("UpOnTheRoofTop.wav")
+    presec = tempo / 2 - .25
+    count = 0
+    os.system('mpg321 upOnTheRoofTop.wav &')
+    while count <= 216:
+        sleep(presec)
+        rando = randint(0, 100)
+        if rando <= 12:
+            slot8(.25)
+        if 12 < rando <= 24:
+            slot7(.25)
+        if 24 < rando <= 36:
+            slot6(.25)
+        if 36 < rando <= 48:
+            slot5(.25)
+        if 48 < rando <= 50:
+            slot4(.25)
+        if 50 < rando <= 62:
+            slot3(.25)
+        if 62 < rando <= 74:
+            slot2(.25)
+        if 74 < rando <= 100:
+            slot1(.25)
+        count += presec
+
+
+rooftop()
